a8a623a04689650377488512e80071183f704f04
chc72331 <chc72331@creighton.edu>
2021-03-16 22:01:09 -0500

    Update membership.js

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,15 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
+
+let members = ["Mary", "Sam", "Joey", "Paul", "Mike",
+                "Char", "Jenny", "Kennedy", "Kaden", 
+                "Kurt", "Anne"]
+
+
+btnMember.onclick=function(){
+  if (members.includes(inptName.value))
+    lblMember.value = 'You\'re already a member.'
+  else {
+    members.push(inptName.value)
+    lblMember.value = 'Congrats! Now you\'re a member.'
+  }
+}
 
